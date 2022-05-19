a8ccf6fb9546ad78501ec64d6372bfdb5171225b
Aman Bisht <amanvista@gmail.com>
2022-05-19 21:13:52 +0530

    7. EJS Working

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,21 @@ const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(bodyParser.json());
-
+app.set('view engine', 'ejs')
 const port = 3000
 
 app.use(express.static(__dirname+'/pages/assets'));
 
+app.get("/ejs", function(req,res){
+    res.render('login')
+})
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/pages/login/index.html");
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/pages/admin_page/admin_page.html");
+  });
 
 
 const authRoute = require('./routes/auth')
